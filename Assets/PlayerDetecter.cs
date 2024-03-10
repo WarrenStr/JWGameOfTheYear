a26b6589f1bf7f90e2d1a1d9917f1f1a19b34b26
WarrenStr <59877718+WarrenStr@users.noreply.github.com>
2024-03-09 21:45:06 -0500

    Level Design

---
+++ Assets/PlayerDetecter.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class PlayerDetecter : MonoBehaviour
+{
+    [Header("Floating Text Strings")]
+    [SerializeField] private string enterFloatingT;
+    [SerializeField] private string exitFloatingT;
+
+    [Header("Command Line Strings")]
+    [SerializeField] private string enterCommandLine;
+    [SerializeField] private string exitCommandLine;
+
+    private bool trigActivated = false;
+
+    public TextMeshProUGUI floatingText;
+    public TextMeshProUGUI textLine;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        floatingText = GetComponentInChildren<TextMeshProUGUI>();
+        floatingText.text = enterFloatingT;
+
+        textLine = GameObject.Find("playerUpdateText").GetComponent<TextMeshProUGUI>();
+        textLine.text = null;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "Player")
+        {
+            if (!trigActivated)
+            {
+                trigActivated = true;
+                if (trigActivated)
+                {
+                    Debug.Log(enterCommandLine);
+                    textLine.text = enterCommandLine;
+                    floatingText.text = exitFloatingT;
+                }
+            }
+
+            else if (trigActivated)
+            {
+                trigActivated = false;
+                if (!trigActivated)
+                {
+                    Debug.Log(exitCommandLine);
+                    textLine.text = exitCommandLine;
+                    floatingText.text = enterFloatingT;
+                }
+            }
+        }
+        
+    }
+}
