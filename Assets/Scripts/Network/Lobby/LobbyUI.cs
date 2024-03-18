9cc7cc71900d00f91aafdacec45f00ecc49f6ed7
Jadlar <jadlarsims11@gmail.com>
2024-03-18 11:21:32 -0400

    map basics
    added UI and basic functionality of map selection.

--- Assets/Scripts/Network/Lobby/LobbyUI.cs
+++ Assets/Scripts/Network/Lobby/LobbyUI.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -8,16 +9,27 @@ public class LobbyUI : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI _lobbyUI;
     [SerializeField] private Button _readyButton;
+    [SerializeField] private Image _mapImage;
+    [SerializeField] private Button _leftButton;
+    [SerializeField] private Button _rightButton;
+    [SerializeField] private TextMeshProUGUI _mapName;
+    [SerializeField] private MapSelectionData _mapSelectionData;
+
+    private int _currentMapIndex = 0;
 
 
     private void OnEnable()
     {
         _readyButton.onClick.AddListener(OnReadyPressed);
+        _leftButton.onClick.AddListener(OnLeftButtonClick);
+        _rightButton.onClick.AddListener(OnRightButtonClick);
     }
 
     private void OnDisable()
     {
         _readyButton.onClick.RemoveAllListeners();
+        _leftButton.onClick.RemoveAllListeners();
+        _rightButton.onClick.RemoveAllListeners();
     }
 
     void Start()
@@ -25,11 +37,36 @@ public class LobbyUI : MonoBehaviour
         _lobbyUI.text = $"Lobby Code: {GameLobbyManager.Instance.GetLobbyCode()}";
     }
 
-
-    void Update()
+    
+    private void OnLeftButtonClick()
     {
-        
+        if (_currentMapIndex - 1 >= 0) 
+        {
+            _currentMapIndex--;
+        }
+        else
+        {
+            _currentMapIndex = _mapSelectionData.Maps.Count - 1; //Make it so it wraps around 
+        }
+
+        UpdateMap();
     }
+
+
+    private void OnRightButtonClick()
+    {
+        if (_currentMapIndex + 1 <= _mapSelectionData.Maps.Count - 1)
+        {
+            _currentMapIndex++;
+        }
+        else
+        {
+            _currentMapIndex = 0;
+        }
+
+        UpdateMap();
+    }
+
 
     private async void OnReadyPressed()
     {
@@ -39,5 +76,11 @@ public class LobbyUI : MonoBehaviour
         {
             _readyButton.gameObject.SetActive(false);
         }
+    }
+
+    private void UpdateMap()
+    {
+        _mapImage.sprite = _mapSelectionData.Maps[_currentMapIndex].MapThumbnail;
+        _mapName.text = _mapSelectionData.Maps[_currentMapIndex].MapName;
     }
 }
