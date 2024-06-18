87e6402400705b4fedf2c00d82a268af1ef67eb2
Jadlar <jadlarsims11@gmail.com>
2024-06-17 21:21:15 -0400

    Lag Fix
    Update lag

---
+++ Assets/Scripts/Mulitplayer/InputState.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//
+
+public class InputState : MonoBehaviour
+{
+    public int Tick;
+    public Vector2 movementInput;
+    public Vector2 lookInput;
+
+
+}
