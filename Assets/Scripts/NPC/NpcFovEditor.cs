7d642f1d315e2c158e9e12a989c0d2733b682cda
Jadlar <jadlarsims11@gmail.com>
2024-08-12 23:13:40 -0400

    AI Update
    AI field of view added to detect collision within its bounds

--- Assets/Scripts/NPC/NpcFovEditor.cs
+++ Assets/Scripts/NPC/NpcFovEditor.cs
@@ -14,7 +14,7 @@ public class NpcFovEditor : Editor
         SimpleNpcFov fov = (SimpleNpcFov)target;
         Handles.color = Color.white;
 
-        //Handles.DrawWireArc(fov.transform.position, Vector3.up, Vector3.forward, 360, fov.viewRadius); // Draw radius.
+        Handles.DrawWireArc(fov.transform.position, Vector3.up, Vector3.forward, 360, fov.viewRadius); // Draw radius.
 
         Vector3 viewAngleA = fov.DirFromAngle(-fov.viewAngle / 2, false);
         Vector3 viewAngleB = fov.DirFromAngle(fov.viewAngle / 2, false);
